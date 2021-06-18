f90ccd8138438c1eaea2c95316dd6cdf5fa31e73
ezequiel_ov <ezequielov@gmail.com>
2021-06-18 12:31:12 -0300

    First Commit (BoardView and InGame)
    +BoardView script to manage the view of the game
    +ChipButton to select in which row player wants to drop the chip
    +InGame script to handle the logic of the game

---
+++ Assets/Scripts/Logic/InGame.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGame : MonoBehaviour
+{
+    int[][] board;
+    [SerializeField] BoardView view;
+    MatchData data;
+    void Start(){
+        data = GetMatchData("MatchData_1");
+        Vector2Int grid = new Vector2Int(data.rows, data.lines);
+        InitBoard(grid);
+        view.Init(grid, OnChipRowSelected);
+    }
+    void Update(){
+        
+    }
+    void InitBoard(Vector2Int grid) {
+        board = new int[grid.x][];
+        for (int x = 0; x < grid.x; x++) {
+            board[x] = new int[grid.y];
+            for(int y = 0; y < grid.y; y++) {
+                board[x][y] = 0;
+            }
+        }
+    }
+    MatchData GetMatchData(string st) {
+        string jsonObj = Resources.Load<TextAsset>("Data/MatchData/" + st).ToString();
+        return JsonUtility.FromJson <MatchData> (jsonObj);
+    }
+    void OnChipRowSelected(int index) {
+        Debug.Log("IN GAME... ROW::: " + index);
+    }
+}
---
+++ Assets/Scripts/View/BoardView.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class BoardView : MonoBehaviour
+{
+    Action<int> onRowSelected;
+    Rect myRect;
+    Button[] selectorRow;
+
+    public void Init(Vector2Int grid, Action<int> onRowSelected) {
+        this.onRowSelected = onRowSelected;
+        myRect = GetComponent<RectTransform>().rect;
+        SetButtons(grid);
+    }
+    void OnChipButtonPressed(int index) { onRowSelected.Invoke(index); }
+    void SetButtons(Vector2Int grid) {        
+        Vector2 boardSize = new Vector2(myRect.width, myRect.height);
+        Button selector = Resources.Load<Button>("Prefabs/ChipButton");
+        selectorRow = new Button[grid.x];
+        for (int i = 0; i < grid.x; i++) {
+            int index = i;
+            selectorRow[i] = Instantiate<Button>(selector);
+            selectorRow[i].transform.parent = transform;
+            float xPos = boardSize.x / grid.x * i + (Screen.width / 2 + myRect.xMin) + ((boardSize.x / grid.x) / 2);
+            selectorRow[i].onClick.AddListener(() => { OnChipButtonPressed(index); });
+            selectorRow[i].GetComponent<RectTransform>().position = new Vector2(xPos, Screen.height * 0.9f);
+        }
+    }
+}
